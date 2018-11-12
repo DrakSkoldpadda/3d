81042ef4416fac6031a6bcab02914b10b3d46d69
Gabriel Leonardsson <gabriel.gonzalezleonardsson@elev.ga.lbs.se>
2018-11-12 19:25:56 +0100

    good

---
+++ Assets/Scripts/CameraControll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+
+        Mouse();
+    }
+
+    void Mouse()
+    {
+        //Press ESC to get the mouse backS
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 10;
+    public float sprintSpeed = 20;
+    private float speed;
+    public float rotatnigSpeed = 300;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Sprint();
+    }
+
+    void Movement()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up * rotatnigSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), Space.World);
+    }
+
+    void Sprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = moveSpeed;
+        }
+    }
+}
